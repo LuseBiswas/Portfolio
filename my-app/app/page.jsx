9485b572385bdef9ef32b841e4be8bc2492edcbd
LuseBiswas <lusebiswas@gmail.com>
2025-10-02 21:37:16 +0530

    Project Setup is done

---
+++ my-app/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+    <h1 className="text-3xl font-extrabold underline text-green-500">
+      Hello world!
+    </h1>
+    </div>
+    </>
+  );
+}
